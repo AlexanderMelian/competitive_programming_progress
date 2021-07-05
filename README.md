77fd58ee7e598e5537bc1db342742526beaf96f0
Alexander Melian <alexandermelian@outlook.com>
2021-07-05 20:31:34 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+### My progress
+
+Hello im Alexander Melian and i like all about this world(competitive programming), i solve problems on leetcode, cses.fi, codeforces and more. \ 
+In this repository i want to show my progress. Thanks
---
+++ template/template.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define ini(x) scanf("%d",&x)
+#define inl(x) scanf("%lld",&x)
+#define ins(s) scanf("%s",s)
+#define pri(x) printf("%d\n",x)
+#define prl(x) printf("%lld\n",x)
+#define prs(s) printf("%s\n",s)
+
+using namespace std;
+using ll = long long;
+
+
+int main(){
+
+}
