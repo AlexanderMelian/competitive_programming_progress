e978b0912dff5c57fefecc5a31fb87a9521c112e
amelian <amelian@primary.com.ar>
2022-01-09 00:12:28 -0300

    fix error

--- template/template.cpp
+++ template/template.cpp
@@ -1,13 +1,14 @@
 #include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
 #define ini(x) scanf("%d",&x)
 #define inl(x) scanf("%lld",&x)
-#define ins(s) scanf("%s",s)
+#define ins(s) getline(cin, s)
 #define pri(x) printf("%d\n",x)
 #define prl(x) printf("%lld\n",x)
 #define prs(s) printf("%s\n",s)
 
-using namespace std;
-using ll = long long;
 
 
 int main(){
