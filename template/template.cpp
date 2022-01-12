78d7e30296027b26c22e42d70410993ef032fcca
amelian <amelian@primary.com.ar>
2022-01-11 23:39:53 -0300

    añadido el clocl

--- template/template.cpp
+++ template/template.cpp
@@ -13,11 +13,26 @@ void solve(){
 
 }
 
+#ifndef CALCULATE_TIME
+void calculate_time(){
+    auto start = clock();
+    solve();
+    auto end = clock();
+    cout << "Time taken: " << end-start << endl;
+}
+#endif
 
 int main(){
     int t;
     ini(t);
+    #ifndef CALCULATE_TIME
+    for(int i=1;i<=t;i++){
+        cout << "Case #" << i << ": ";
+        solve();
+    }
+    #else
     while(t--){
         solve();
     }
+    #endif
 }
